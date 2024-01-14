b61b76416d2ac5c7f111f34ed2f5a91135e1c7a7
OwenLu <gg0125gg@gmail.com>
2024-01-15 00:01:57 +0800

    feat: add example button

--- dapp/pages/index.tsx
+++ dapp/pages/index.tsx
@@ -1,10 +1,10 @@
-import { SetStateAction, useState, useEffect } from 'react';
+import { SetStateAction, useState } from 'react';
 import styles from '../styles/Home.module.css';
 import type { NextPage } from 'next';
 import Image from 'next/image'
 import Head from 'next/head';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-import { Container, Paper, TextField, Button } from '@mui/material';
+import { Container, Paper, TextField, Button, Typography, Grid, Box } from '@mui/material';
 import NFTCard from '../components/NFTCard';
 import BouncingBall from '../components/BouncingBall';
 
@@ -15,10 +15,6 @@ const Home: NextPage = () => {
   const SendAddress = () => {
     setAdd(addressInput);
   };
-
-  useEffect(() => {
-    console.log(addressInput);
-  }, [addressInput]);
 
   return (
     <div className={styles.container}>
@@ -53,23 +49,42 @@ const Home: NextPage = () => {
             height={100}
             style={{ marginBottom: '15px', borderRadius: '20px' }}
           ></Image>
-          <ConnectButton />
           <Container sx={{ py: 2 }} maxWidth="md">
-            <TextField fullWidth label="type address" id="123"
+            <Grid container justifyContent="center" alignItems="center">
+              <ConnectButton />
+            </Grid>
+            <TextField fullWidth label="type address" id="addInput"
               sx={{
-                mb: '15px'
+                mb: '15px',
+                mt: '15px'
               }}
+              value={addressInput}
               onChange={(e: { target: { value: SetStateAction<string> } }) => {
                 if (e.target.value.toString().startsWith("0x")) {
                   setAddressInput(e.target.value);
                 }
+                setAddressInput(e.target.value);
               }}
             />
+            <Box
+              sx={{ mb: '10px' }}
+            >
+              <Typography
+                display={'inline'}
+              >
+                Example:
+              </Typography>
+              <Button onClick={() => {
+                setAddressInput('0xd8FDf15e99b371214D6f1728C85f96635361978c');
+              }}>address 1</Button>
+              <Button onClick={() => {
+                setAddressInput('0x448C510Ada54D79701112e9aAf8F3d3Eab60CB48');
+              }}>address 2</Button>
+            </Box>
             <Button fullWidth variant="outlined" sx={{
               mb: '15px'
             }}
               onClick={SendAddress} >Fetch
-
             </Button>
             <NFTCard ethAddress={add || ''} />
           </Container>
