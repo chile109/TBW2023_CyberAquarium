16dd2153e26b979b452902901569312d219961b3
OwenLu <gg0125gg@gmail.com>
2024-01-19 17:32:47 +0800

    feat: main page adjust

--- dapp/pages/index.tsx
+++ dapp/pages/index.tsx
@@ -1,15 +1,18 @@
 import { SetStateAction, useEffect, useState } from 'react';
-import styles from '../styles/Home.module.css';
 import type { NextPage } from 'next';
 import Image from 'next/image'
 import Head from 'next/head';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { Container, Paper, TextField, Button, Typography, Grid, Box, colors } from '@mui/material';
-import NFTCard from '../components/NFTCard';
-import BouncingBall from '../components/BouncingBall';
 import { useWalletClient } from 'wagmi'
 import { TokenboundClient } from '@tokenbound/sdk'
 import { type TBAccountParams } from "@tokenbound/sdk/dist/src";
+import AquariumBag from '../components/AquariumBag';
+import SidebarMenu from '../components/SidebarMenu';
+import WrapWalletLink from '../components/WrapWalletLink';
+import { useGetNFT } from '../hooks/openSeaApi';
+import NFTCard from '../components/NFTCard';
+import BouncingBall from '../components/BouncingBall';
 
 const DEFAULT_ACCOUNT: TBAccountParams = {
   tokenContract: "0x",
@@ -17,6 +20,12 @@ const DEFAULT_ACCOUNT: TBAccountParams = {
 }
 
 const Home: NextPage = () => {
+  const chain = 'sepolia'
+  const identifier = 1
+  const _address = '0x6CcA2d398B2060DC824ba0Cdaf69a8e8344C329e'
+  const fishAddress = '0x51840Ea7B892145feaCB347Ab2ebaac9032A2140'
+  const { nfts } = useGetNFT(chain, identifier, _address)
+  const [isOpen, setIsOpen] = useState(true);
   const [addressInput, setAddressInput] = useState<string>('');
   const [add, setAdd] = useState('');
   const [tbaTokenId, setTbaTokenId] = useState<number>(0);
@@ -51,9 +60,13 @@ const Home: NextPage = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [TBAccount]);
 
+  if (nfts === undefined) {
+    return null;
+  }
+
   return (
-    <div className={styles.container}>
-      <BouncingBall ethAddress={add || ''} />
+    <main className="...">
+      <BouncingBall ethAddress={addressInput || ''} />
       <Head>
         <title>CyberAquarium</title>
         <meta
@@ -62,91 +75,97 @@ const Home: NextPage = () => {
         />
         <link href="/favicon.ico" rel="icon" />
       </Head>
-      <Image
-        src="/aquarium/aquarium-10.jpeg"
-        alt="bg"
+      <Image src={nfts.image_url as any} alt={''}
         layout='fill'
-        z-tabindex={-99}
-        style={{ position: 'absolute', zIndex: -2 }}
-      />
-      <main className={styles.main}>
-        <Paper
-          elevation={6}
-          sx={{
-            width: '45%',
-            padding: '2rem',
-            maxHeight: '80vh',
-            background: 'linear-gradient(180deg, rgba(34, 36, 80, 0.9) 0%, rgba(23, 24, 38, 0.9) 100%)',
-            border: '3px solid #6DCDFF',
-            borderRadius: '1rem',
-            overflow: 'auto',
-            '&::-webkit-scrollbar': {
-              display: 'none'
-            },
-          }}>
-          <Image
-            src='/logo-002.png'
-            alt='logo'
-            layout='responsive'
-            width={100}
-            height={100}
-            style={{ marginBottom: '15px', borderRadius: '20px' }}
-          ></Image>
-          <Container sx={{ py: 2 }} maxWidth="md">
-            <Grid container justifyContent="center" alignItems="center">
-              <ConnectButton />
-            </Grid>
-            <TextField fullWidth label="Type TBA address" id="addInput"
-              InputLabelProps={{ style: { color: 'white' } }}
-              color='secondary'
+        style={{ zIndex: -99, }}
+      ></Image>
+      <div className="...">
+        <div onClick={() => setIsOpen(!isOpen)}>
+          <WrapWalletLink />
+        </div>
+        <AquariumBag />
+        <SidebarMenu signer={undefined} />
+        {isOpen ? (
+          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <Paper
+              elevation={6}
               sx={{
-                mb: '15px',
-                mt: '15px',
-              }}
-              value={addressInput}
-              inputProps={{
-                style: { color: 'white' },
-              }}
-              onChange={(e: { target: { value: SetStateAction<string> } }) => {
-                if (e.target.value.toString().startsWith("0x")) {
-                  setAddressInput(e.target.value);
-                }
-                setAddressInput(e.target.value);
-              }}
-            />
-            <Box
-              sx={{ mb: '10px' }}
-            >
-              <Typography
-                display={'inline'}
-                sx={{
-                  color: 'white'
+                width: '45%',
+                padding: '2rem',
+                maxHeight: '80vh',
+                background: 'linear-gradient(180deg, rgba(34, 36, 80, 0.9) 0%, rgba(23, 24, 38, 0.9) 100%)',
+                border: '3px solid #6DCDFF',
+                borderRadius: '1rem',
+                overflow: 'auto',
+                '&::-webkit-scrollbar': {
+                  display: 'none'
+                },
+              }}>
+              <Image
+                src='/logo-002.png'
+                alt='logo'
+                layout='responsive'
+                width={100}
+                height={100}
+                style={{ marginBottom: '15px', borderRadius: '20px' }}
+              ></Image>
+              <Container sx={{ py: 2 }} maxWidth="md">
+                <Grid container justifyContent="center" alignItems="center">
+                  <ConnectButton />
+                </Grid>
+                <TextField fullWidth label="Type TBA address" id="addInput"
+                  InputLabelProps={{ style: { color: 'white' } }}
+                  color='secondary'
+                  sx={{
+                    mb: '15px',
+                    mt: '15px',
+                  }}
+                  value={addressInput}
+                  inputProps={{
+                    style: { color: 'white' },
+                  }}
+                  onChange={(e: { target: { value: SetStateAction<string> } }) => {
+                    if (e.target.value.toString().startsWith("0x")) {
+                      setAddressInput(e.target.value);
+                    }
+                    setAddressInput(e.target.value);
+                  }}
+                />
+                <Box
+                  sx={{ mb: '10px' }}
+                >
+                  <Typography
+                    display={'inline'}
+                    sx={{
+                      color: 'white'
+                    }}
+                  >
+                    Example:
+                  </Typography>
+                  <Button onClick={() => {
+                    setAddressInput('0xd8FDf15e99b371214D6f1728C85f96635361978c');
+                  }}>address 1</Button>
+                  <Button onClick={() => {
+                    setAddressInput('0x51840Ea7B892145feaCB347Ab2ebaac9032A2140');
+                  }}>address 2</Button>
+                </Box>
+                {/* <Button fullWidth variant="outlined" sx={{
+                  mb: '15px'
                 }}
-              >
-                Example:
-              </Typography>
-              <Button onClick={() => {
-                setAddressInput('0xd8FDf15e99b371214D6f1728C85f96635361978c');
-              }}>address 1</Button>
-              <Button onClick={() => {
-                setAddressInput('0x51840Ea7B892145feaCB347Ab2ebaac9032A2140');
-              }}>address 2</Button>
-            </Box>
-            <Button fullWidth variant="outlined" sx={{
-              mb: '15px'
-            }}
-              onClick={() => setAdd(addressInput)} >Fetch fish
-            </Button>
-            <Typography variant='h3' align='center' sx={{
-              color: 'white',
-              mb: '1rem'
-            }}>My Aquarium</Typography>
-            <NFTCard ethAddress={''} onTbaAddChange={setTbaTokenId} />
-          </Container>
-        </Paper>
-      </main>
-    </div >
-  );
-};
+                  onClick={() => setAdd(addressInput)} >Fetch fish
+                </Button> */}
+                <Typography variant='h3' align='center' sx={{
+                  color: 'white',
+                  mb: '1rem'
+                }}>My Aquarium</Typography>
+                <NFTCard ethAddress={''} onTbaAddChange={setTbaTokenId} />
+              </Container>
+            </Paper>
+          </Box>
+        ) : null}
+      </div>
+    </main>
+  )
+}
 
 export default Home;
