bca493a94a68dc976e108e6e0f21ea98088994a6
OwenLu <gg0125gg@gmail.com>
2024-01-19 14:32:15 +0800

    chore: add OpenSeaApi

---
+++ dapp/hooks/openSeaApi.ts
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+
+type nftsData = {
+  identifier: number;
+  image_url: string;
+}
+
+export const useGetNFT = (chain: string,identifier: string,address?: string) => {
+  const [nfts, setNfts] = useState<nftsData>();
+  useEffect(() => {
+    fetch(
+      `https://testnets-api.opensea.io/api/v2/chain/${chain}/contract/${address}/nfts/${identifier}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setNfts(data.nft);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [identifier]);
+
+  return { nfts };
+};
+
+export const GetNFTsByAccount = (chain: string,identifier: string,address?: string) => {
+  const [nfts, setNfts] = useState([]);
+  useEffect(() => {
+    fetch(
+      `https://api.opensea.io/api/v2/chain/${chain}/account/${address}/nfts`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setNfts(data.nfts || []);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [identifier]);
+
+  return { nfts };
+};
